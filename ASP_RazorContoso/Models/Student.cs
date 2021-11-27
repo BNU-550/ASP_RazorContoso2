a331bbaa93e79d26a7203978b211bd1425d64ba4
kayleysyrett <73078305+kayleysyrett@users.noreply.github.com>
2021-11-27 11:10:08 +0000

    Add project files.

---
+++ ASP_RazorContoso/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+
+namespace ASP_RazorContoso.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Enrol Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        // Navigation Property
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+
+        public string FullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
